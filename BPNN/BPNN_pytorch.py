0a53e0889aa6f3144f37fd044d6ec89058aa2ad3
lyx <765020742@qq.com>
2020-01-23 19:06:51 +0800

    新增BP神经网络

---
+++ BPNN/BPNN_pytorch.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2020/1/19 10:56
+# @Author : LYX-夜光
+
+import torch
+import numpy as np
+from collections import OrderedDict
+
+# 神经网络
+def networks(x, y):
+    # 数据转化为张量
+    x = torch.tensor(x).float()
+    y = torch.tensor(y).float()
+
+    input_data = x.shape[1]  # 输入（特征）个数
+    hidden_layer = x.shape[1]  # 隐藏层个数
+    output_data = y.shape[1]  # 输出个数
+
+    # 神经网络模型
+    model = torch.nn.Sequential(OrderedDict([
+        ("Line1", torch.nn.Linear(input_data, hidden_layer)),  # 输入层至隐藏层的计算
+        ("Sigm1", torch.nn.Sigmoid()),  # 激活函数
+        ("Line2", torch.nn.Linear(hidden_layer, output_data)),  # 隐藏层至输出层的计算
+        ("Sigm2", torch.nn.Sigmoid()),  # 激活函数
+    ]))
+
+    learningRate = 0.5  # 训练参数
+    epochs = 2000  # 迭代次数
+    loss_fn = torch.nn.MSELoss()  # 损失函数
+
+    for epoch in range(epochs):
+        yPred = model(x)
+        loss = loss_fn(yPred, y)
+        if epoch % 100 == 0:
+            print("Epoch %d loss: %.3f" % (epoch, loss.data))
+        model.zero_grad()  # 求解梯度前需要清空之前的梯度结果（因为model会累加梯度）
+        loss.backward()  # 梯度计算
+        for param in model.parameters():
+            param.data -= param.grad.data * learningRate  # 调整参数
+    return model
+
+
+if __name__ == "__main__":
+    data = np.array([[133, 65],  # 每一行是体重（磅）和身高（英寸）
+                     [160, 72],
+                     [152, 70],
+                     [120, 60]])
+    maxD, minD = data.max(0), data.min(0)
+    newData = (data - minD) / (maxD - minD)  # 单位化
+    yTrues = np.array([[0, 1, 1, 0]]).T  # 1男性 0女性
+
+    model = networks(newData, yTrues)
+
+    # 测试样例
+    Alice = np.array([128, 63])
+    dataEmily = torch.tensor(((Alice - minD) / (maxD - minD))).float()
+    gender = model.forward(dataEmily)
+    print("Alice: %f" % gender)
+
+    Frank = np.array([155, 68])
+    dataFrank = torch.tensor(((Frank - minD) / (maxD - minD))).float()
+    gender = model.forward(dataFrank)
+    print("Frank: %f" % gender)
---
+++ BPNN/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2020/1/23 18:43
+# @Author : LYX-夜光
