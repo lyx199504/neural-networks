de89ab0a326b47ff6957d803a859156441fedca0
lyx <765020742@qq.com>
2020-02-11 18:20:05 +0800

    修改三元损失

--- CNN/faceRecognition/getFaceDatas.py
+++ CNN/faceRecognition/getFaceDatas.py
@@ -3,11 +3,11 @@
 # @Time : 2020/2/8 13:33
 # @Author : LYX-夜光
 
+import torch
 from torchvision import transforms
 from torch.utils.data import Dataset, DataLoader
 from PIL import Image
 import os
-import random
 
 # 获取数据集类
 class FaceDataset(Dataset):
@@ -33,33 +33,30 @@ class FaceDataset(Dataset):
         label = int(index / self.numPerLabel)
         return data, label
 
-# 获取三元组数据集
-class TripleDataset(Dataset):
-    def __init__(self, rawDataset, numPerLabel=20):
-        self.dataset = []  # 生成三元组数据集
-        anchor_index = 0  # 选择第1张作为主图像
-        for i in range(1, numPerLabel):  # 剩下19张作为positive
-            positive_index = i
-            randList = list(range(numPerLabel, len(rawDataset)))
-            random.shuffle(randList)
-            negativeNum = 250  # 随机取positiveNum张作为negative
-            for j in randList[:negativeNum]:
-                negative_index = j
-                # # 检查选取的三元组
-                # print(rawDataset[anchor_index][1], rawDataset[positive_index][1], rawDataset[negative_index][1])
-                self.dataset.append([rawDataset[anchor_index][0], rawDataset[positive_index][0], rawDataset[negative_index][0]])
-
-    def __len__(self):
-        return len(self.dataset)
-
-    def __getitem__(self, index):
-        return self.dataset[index][0], self.dataset[index][1], self.dataset[index][2]
-
 # 获取训练集
-def getTrainDataSet(batch_size=10, transform=None, numPerLabel=20):
+def getTrainDataSet(model, device, batch_size=10, transform=None, numPerLabel=20):
     rawDataset = FaceDataset('./data/train', transform=transform, numPerLabel=numPerLabel)
     # 选择三元组数据
-    train_dataSet = TripleDataset(rawDataset, numPerLabel)
+    image = torch.tensor([rawDataset[0][0].tolist()]).to(device)
+    with torch.no_grad():
+        anchor = model(image).cpu()  # anchor图像向量
+    # 创建三元hard数据集
+    distList = []  # negative与anchor的距离
+    for i in range(numPerLabel, len(rawDataset)):
+        image = torch.tensor([rawDataset[i][0].tolist()]).to(device)
+        with torch.no_grad():
+            negative = model(image).cpu()  # negative图像向量
+        dist = torch.dist(negative, anchor)  # 欧式距离
+        distList.append([i, dist])
+    distList = sorted(distList, key=lambda x: x[-1])  # 距离从小到大排序
+    negativeNum = 200  # 取出与anchor距离最小的negativeNum个下标（取200个是因为电脑配置有限）
+    negativeIndexList = list(map(lambda x: x[0], distList[:negativeNum]))
+    train_dataSet = []  # 三元hard数据集
+    for positive_index in range(1, numPerLabel):  # positive
+        for negative_index in negativeIndexList:  # negative
+            train_dataSet.append([rawDataset[0][0], rawDataset[positive_index][0], rawDataset[negative_index][0]])
+            # # 检查选取的三元数据
+            # print(rawDataset[0][1], rawDataset[positive_index][1], rawDataset[negative_index][1])
     train_loader = DataLoader(train_dataSet, batch_size=batch_size, shuffle=True, num_workers=2)
     return train_loader
 
@@ -67,5 +64,10 @@ def getTrainDataSet(batch_size=10, transform=None, numPerLabel=20):
 def getTestDataSet(batch_size=4, transform=None, numPerLabel=10):
     test_dataSet = FaceDataset('./data/test', transform=transform, numPerLabel=numPerLabel)
     test_loader = DataLoader(test_dataSet, batch_size=batch_size, shuffle=False, num_workers=2)
-    anchors = DataLoader([test_dataSet[0]], batch_size=1, shuffle=False)
-    return test_loader, anchors
+    return test_loader, test_dataSet[0]
+
+# 获取验证集（没有验证集，用训练集）
+def getVerifyDataSet(batch_size=4, transform=None, numPerLabel=20):
+    verify_dataSet = FaceDataset('./data/train', transform=transform, numPerLabel=numPerLabel)
+    verify_loader = DataLoader(verify_dataSet, batch_size=batch_size, shuffle=False, num_workers=2)
+    return verify_loader, verify_dataSet[0]
--- CNN/faceRecognition/train.py
+++ CNN/faceRecognition/train.py
@@ -11,7 +11,7 @@ from CNN.faceRecognition import getFaceDatas
 # 训练
 def train(model, datas, device, learning_rate=0.0005):
     model.train()  # 训练模式开启
-    triple_loss_fn = torch.nn.TripletMarginLoss(margin=100.0, p=2)
+    triple_loss_fn = torch.nn.TripletMarginLoss(margin=2.0, p=2)
     optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9)
 
     for i, data in enumerate(datas, 0):
@@ -19,19 +19,22 @@ def train(model, datas, device, learning_rate=0.0005):
         anchor_out, positive_out, negative_out = model(anchor), model(positive), model(negative)
         loss = triple_loss_fn(anchor_out, positive_out, negative_out)
         if i % 10 == 9:
-            print("No.%03d loss: %.6f" % (i+1, float(loss)))
+            print("第%d次训练的三元损失值: %.6f" % (i+1, float(loss)))
         optimizer.zero_grad()  # 求解梯度前需要清空之前的梯度结果（因为model会累加梯度）
         loss.backward()  # 梯度计算
         optimizer.step()  # 优化更新权值
     return model
 
 if __name__ == "__main__":
+    '''
+    设备有限，只让模型认识 Alejandro_Toledo 这个人，即模型只能识别 Alejandro_Toledo 的人脸
+    '''
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     dir = "./model"
     try:
-        fileName = sorted(os.listdir(dir), reverse=True)[0]
-        model_in = dir + '/' + fileName
-        model_out = dir + "/model_%03d.pth" % (int(fileName.split('.')[0].split('_')[1])+1)
+        fileName = sorted(os.listdir(dir), reverse=True)[0]  # 尝试读取模型
+        model_in = dir + '/' + fileName  # 输入模型的文件名
+        model_out = dir + "/model_%03d.pth" % (int(fileName.split('.')[0].split('_')[1])+1)  # 输出模型的文件名
     except:
         model_in = None
         model_out = dir + "/model_001.pth"
@@ -40,8 +43,9 @@ if __name__ == "__main__":
         model = torch.load(model_in).to(device)
     else:  # 模型不存在，则选择预训练模型
         model = torchvision.models.resnet18(pretrained=True).to(device)
+        model.fc = torch.nn.Softmax(1)
     print("开始加载数据。。。")
-    train_loader = getFaceDatas.getTrainDataSet()
+    train_loader = getFaceDatas.getTrainDataSet(model, device)
     print("开始训练模型。。。")
     model = train(model, train_loader, device).cpu()
     torch.save(model, model_out)
